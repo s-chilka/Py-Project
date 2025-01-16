d4588bf3fdbbe8f2ef5d796be7d14c877759ac96
s-chilka <43394519+s-chilka@users.noreply.github.com>
2025-01-16 13:57:34 -0800

    Update Icon.py
    FrontEnd Design Development Tool

--- Icon/Icon.py
+++ Icon/Icon.py
@@ -1,1 +1,17 @@
+"""
+# File: Icon.py
+# Author: schilka
+# Date: 2025-01-16
 
+# Description:
+# This is an auto RTL wrapper generator Tool.
+# FrontEnd Design and development with scalable feature extensions
+
+#Authorized Usage: 
+# This file is licensed to {{ licensee_name }} for use solely in accordance with the 
+# terms and conditions of the license agreement. Unauthorized copying or distribution 
+# of this file, via any medium, is prohibited.
+    
+# Usage:
+#  TBD
+"""
